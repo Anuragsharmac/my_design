19234c716bff6d43ec96f74bd1fe8cc5eeacb467
Anurag gautam <anuragbro00001@gmail.com>
2023-11-16 16:33:21 +0530

    initial commit

---
+++ array.js
@@ -0,0 +1,35 @@
+// let arr = [10,20,30,40,50,60,70,80,90];
+
+// for(let a of arr){
+//     console.log(a);
+// }
+
+// arr.forEach(num => {
+//     console.log(num);
+// })
+
+// for(let i=0;i<=6;i++)
+// {
+//     console.log(arr[i]);
+// }
+
+// arr.filter( (num) => {
+//     if(num>60){
+//         console.log(num);
+//     }
+// })
+// let b = arr.map((num) => {
+//    return num*2;
+// });
+// console.log(b);
+
+// let person = 
+//     {fname:'Anurag',
+//      lname:'Gautam'
+//     };
+
+// function fullName(){
+//    return `${person.fname} ${person.lname}`
+// }
+
+// console.log(fullName());
